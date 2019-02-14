4fc4f025c2179b0128724ad943099c23ed7ed239
wangyue <wangyue2@able-elec.com>
2019-02-14 10:39:25 +0800

    first

--- cmdb/views.py
+++ cmdb/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .models import project,ecslist
 import datetime
+import pika
+
 from queue import Queue
 
+
+username='wangyue'
+passwd='wangyue'
+rabbitinfo=pika.PlainCredentials(username,passwd)
 # Create your views here.
 def index(request):
     return render(request, 'cmdb/css-bak.html')
@@ -16,10 +22,20 @@ def testinfo(request):
     service = project.objects.get(pro_name=request.GET['service'])
     hosts_list = [i.ip for i in service.ecslist_set.all()]
     print(hosts_list)
-    filetime = datetime.datetime.now().strftime('%H-%M-%S')
+    filetime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     with open(filetime+'-hosts','wt') as hostsfile:
         for i in hosts_list:
             hostsfile.write(i+' ansible_ssh_user=root ansible_ssh_pass="Vpcecs@2018!q@w#" ansible_ssh_port=22' + '\n')
+
+    mqinfo = "{'service':" + "'" + str(request.GET['service']) + "'" + ",'action':" + "'" + str(
+        request.GET['action']) + "'" + ",'file':" + "'" + str(filetime + '-hosts') + "'}"
+    connection = pika.BlockingConnection(pika.ConnectionParameters('172.22.0.69', credentials=rabbitinfo))
+    channel = connection.channel()
+    channel.queue_declare(queue='yunwei-uV', durable=True)
+    channel.basic_publish(exchange='', routing_key='yunwei-uV', body=mqinfo)
+    print('消息发送到rabbit:{}'.format(mqinfo))
+    connection.close()
+
 
     return HttpResponse('嘻嘻')
 
--- ops/urls.py
+++ ops/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cmdb/',include('cmdb.urls')),
+    path('cmdb/',include('cmdb.urls',namespace='cmdb')),
 ]
